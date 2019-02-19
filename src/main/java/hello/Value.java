76fac0b5454870bc2656a29c6d5ab3964b77feee
Noel Vock <noel1uk@gmail.com>
2019-02-19 00:21:08 +0000

    add class for embedding the inner quotation

--- src/main/java/hello/Value.java
+++ src/main/java/hello/Value.java
@@ -1,4 +1,37 @@
 package hello;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties
 public class Value {
+
+    private Long id;
+    private String quote;
+
+    public Value() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    @Override
+    public String toString() {
+        return "Value{" +
+                "id=" + id +
+                ", quote='" + quote + '\'' +
+                '}';
+    }
 }
